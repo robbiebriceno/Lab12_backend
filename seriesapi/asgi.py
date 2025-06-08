5e47628a0a3822d71889f2d37318ba95be82be91
robbiebriceno <robbi.briceno@tecsup.edu.pe>
2025-06-07 19:23:24 -0500

    commit 1

--- seriesapi/asgi.py
+++ seriesapi/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for series_api project.
+ASGI config for seriesapi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'series_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seriesapi.settings')
 
 application = get_asgi_application()
--- seriesapi/wsgi.py
+++ seriesapi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for series_api project.
+WSGI config for seriesapi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'series_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seriesapi.settings')
 
 application = get_wsgi_application()
